e648009da06ac6e02bcd90a85b629385643ecf68
alecanonm <al3jandrocan0n@gmail.com>
2023-10-06 22:51:55 -0500

    almost the calculator done

--- index.js
+++ index.js
@@ -1,18 +1,90 @@
-const display = document.querySelector(".screen");
-const clean = document.querySelector(".clean");
-const back = document.querySelector(".arrow");
-const module = document.querySelector(".module");
-const seven = document.querySelector(".seven");
-const eight = document.querySelector(".eight");
-const nine = document.querySelector("nine");
-const x = document.querySelector(".x");
-const four = document.querySelector(".four");
-const five = document.querySelector(".five");
-const six = document.querySelector(".six");
-const minus = document.querySelector(".minus");
-const one = document.querySelector(".one");
-const two = document.querySelector("two");
-const three = document.querySelector("three");
-const plus = document.querySelector("plus");
-const zero = document.querySelector(".number-0");
-const equal = document.querySelector(".equal");
+let intBuffer = 0;
+let runningTotal = 0;
+let previousOperator = null;
+const screen = document.querySelector(".screen");
+
+const buttonClick = (value) => {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  updateScreen(intBuffer);
+};
+
+const handleNumber = (value) => {
+  if (intBuffer === 0) {
+    intBuffer = value;
+  } else {
+    intBuffer += value;
+  }
+};
+
+const handleSymbol = (value) => {
+  switch (value) {
+    case "C":
+      intBuffer = 0;
+      break;
+    case "←":
+      if (intBuffer.length === 1) {
+        intBuffer = 0;
+      } else {
+        intBuffer = intBuffer.substring(0, intBuffer.length - 1);
+      }
+      break;
+    case "+":
+    case "-":
+    case "×":
+    case "÷":
+      handleMath(value);
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(intBuffer));
+      previousOperator = null;
+      intBuffer = +runningTotal;
+      runningTotal = 0;
+      break;
+  }
+};
+
+const handleMath = (value) => {
+  if (intBuffer === 0) {
+    return;
+  }
+  const intBufferValue = parseInt(intBuffer);
+  if (runningTotal === 0) {
+    runningTotal = intBufferValue;
+  } else {
+    flushOperation(intBufferValue);
+  }
+  previousOperator = value;
+  intBuffer = 0;
+};
+
+const flushOperation = (intBufferValue) => {
+  if (previousOperator === "+") {
+    runningTotal += intBufferValue;
+  } else if (previousOperator === "-") {
+    runningTotal -= intBufferValue;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBufferValue;
+  } else if (previousOperator === "÷") {
+    runningTotal /= intBufferValue;
+  }
+};
+
+function start() {
+  console.log("starting the calc");
+  document.querySelector(".calculator").addEventListener("click", (event) => {
+    buttonClick(event.target.innerText);
+  });
+}
+
+function updateScreen() {
+  screen.innerText = intBuffer;
+}
+
+start();
